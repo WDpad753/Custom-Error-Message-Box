6c84e2d2c79b50910f42b240c820ea9ae96071be
Mohamed Ahmed <mohamed.ahmed@s3-id.com>
2025-08-04 08:44:04 +0100

    Updated the projects to fix errors in build

--- CustomErrorMessageBox/MVVM/Views/ErrorMessageBox/BaseErrorMessageBox.xaml.cs
+++ CustomErrorMessageBox/MVVM/Views/ErrorMessageBox/BaseErrorMessageBox.xaml.cs
@@ -36,7 +36,7 @@ namespace CustomErrorMessageBox.MVVM.Views.ErrorMessageBox
             InitializeComponent();
         }
 
-        public void ShowErrorMsg(string message, Exception ex)
+        public void Show(string message, Exception ex)
         {
             var buttons = new List<DialogButtons>()
             {
